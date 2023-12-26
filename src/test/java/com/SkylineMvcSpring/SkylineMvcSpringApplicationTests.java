835a36af5afe00c47278eb0440962a2db7912e67
AdrianRoger <114889467+AdrianRoger@users.noreply.github.com>
2023-12-26 01:38:47 -0300

    [feat]:Start - Creating Skyline refactor project folder

---
+++ src/test/java/com/SkylineMvcSpring/SkylineMvcSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SkylineMvcSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkylineMvcSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
