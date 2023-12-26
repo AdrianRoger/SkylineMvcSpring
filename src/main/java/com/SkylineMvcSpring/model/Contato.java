fefc761f331ecfed2344fbbc3118ed7938d9059a
AdrianRoger <114889467+AdrianRoger@users.noreply.github.com>
2023-12-26 13:31:11 -0300

    [feat]: create Contato model and Repository

---
+++ src/main/java/com/SkylineMvcSpring/model/Contato.java
@@ -0,0 +1,122 @@
+package com.SkylineMvcSpring.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Contato {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	@Column(nullable = false)
+	private String telefone;
+	
+	@Column(nullable = false)
+	private String mensagem;
+	
+	@Column(columnDefinition = "bollean default false")
+	private String resolvido;
+	
+	public Contato() {
+		
+	}
+
+	public Contato(Long id, String nome, String email, String telefone, String mensagem, String resolvido) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.mensagem = mensagem;
+		this.resolvido = resolvido;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+
+	public String getResolvido() {
+		return resolvido;
+	}
+
+	public void setResolvido(String resolvido) {
+		this.resolvido = resolvido;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, mensagem, nome, resolvido, telefone);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(mensagem, other.mensagem) && Objects.equals(nome, other.nome)
+				&& Objects.equals(resolvido, other.resolvido) && Objects.equals(telefone, other.telefone);
+	}
+
+	@Override
+	public String toString() {
+		return "Contato [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + ", mensagem="
+				+ mensagem + ", resolvido=" + resolvido + "]";
+	}
+	
+}
