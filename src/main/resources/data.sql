5e47bb103dae9803d431d71cdb45cde5600dc641
AdrianRoger <114889467+AdrianRoger@users.noreply.github.com>
2023-12-28 00:27:34 -0300

    Empty commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,15 +2,15 @@
 
 insert into usuario(cpf, nome, email, telefone, cep, rua, numero, complemento, ativo)
 	values("000.000.000-01", "Adrian Roger", "adrian.santos@aluno.recode.org.br", 
-	"61995034528", "72145-000", "Qnm 38", 46, null, true);
+	"(61)995034528", "72145-000", "Qnm 38", 46, null, true);
 
 insert into usuario(cpf, nome, email, telefone, cep, rua, numero, complemento, ativo)
 	values("000.000.000-02", "Lucas", "lucas@gmail.com", 
-	"61999999999", "00000-000", "Sem nome", 01, null, true);
+	"(61)999999999", "00000-000", "Sem nome", 01, null, true);
 	
 insert into usuario(cpf, nome, email, telefone, cep, rua, numero, complemento, ativo)
 	values("000.000.000-03", "Marco Aurélio", "marco.aurelio@gmail.com", 
-	"61987654321", "72000-000", "Qnm 40 conj C", 35, null, true);
+	"(61)987654321", "72000-000", "Qnm 40 conj C", 35, null, true);
 
 /*Cidade*/
 
@@ -61,11 +61,11 @@ insert into reserva(id, data_reserva, num_pessoas, cancelada, usuario_cpf, voo_i
 /*Contato*/
 
 insert into contato(id, nome, email, telefone, mensagem, resolvido)
-	values(null, "Altair Martins", "altair@gmail.com", "1140028922", "Solicito contato para esclarecimento de preços", false);
+	values(null, "Altair Martins", "altair@gmail.com", "(11)40028922", "Solicito contato para esclarecimento de preços", false);
 	
 insert into contato(id, nome, email, telefone, mensagem, resolvido)
-	values(null, "Rosa Maria", "rosa@gmail.com", "0112345678", "Preciso de informações sobre passagens para PCD", false);
+	values(null, "Rosa Maria", "rosa@gmail.com", "(01)12345678", "Preciso de informações sobre passagens para PCD", false);
 	
 insert into contato(id, nome, email, telefone, mensagem, resolvido)
-	values(null, "George Mota", "mota@gmail.com", "01133565896", "Preciso tirar dúvidas referentes às formas de pagamento", false);
-	
+	values(null, "George Mota", "mota@gmail.com", "(11)33565896", "Preciso tirar dúvidas referentes às formas de pagamento", false);
+
