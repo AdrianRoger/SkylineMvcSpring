82c8fbbab7fc4b3713b31afc744cfc1b4dcc1c93
AdrianRoger <114889467+AdrianRoger@users.noreply.github.com>
2023-12-28 21:43:07 -0300

    [feat]:Create cidadeController and refactor view admin/cidade
        more: some files renamed to meed project standard
    
        On branch main
        Changes to be committed:
            new file:   src/main/java/com/SkylineMvcSpring/controller/CidadeController.java
            renamed:    src/main/resources/templates/admin/cidades.html -> src/main/resources/templates/admin/cidade.html
            renamed:    src/main/resources/templates/admin/clientes.html -> src/main/resources/templates/admin/cliente.html
            modified:   src/main/resources/templates/admin/contato.html
            renamed:    src/main/resources/templates/admin/reservas.html -> src/main/resources/templates/admin/reserva.html
            modified:   src/main/resources/templates/admin/voo.html
            modified:   src/main/resources/templates/contato.html
            modified:   src/main/resources/templates/destino.html
            modified:   src/main/resources/templates/index.html
            modified:   src/main/resources/templates/promocoes.html

---
+++ src/main/java/com/SkylineMvcSpring/controller/CidadeController.java
@@ -0,0 +1,71 @@
+package com.SkylineMvcSpring.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.SkylineMvcSpring.model.Cidade;
+import com.SkylineMvcSpring.repository.CidadeRepository;
+
+@Controller
+@RequestMapping("/admin/cidade")
+public class CidadeController {
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@GetMapping
+	public ModelAndView list() throws IOException{
+		ModelAndView modelAndView = new ModelAndView("/admin/cidade");
+		
+		List<Cidade> cidades = cidadeRepository.findAll();
+		modelAndView.addObject("cidades", cidades);
+		
+		Cidade cidade = new Cidade();
+		modelAndView.addObject("cidade", cidade);
+		
+		return modelAndView;
+	}
+	
+	@PostMapping
+	public ModelAndView create(@ModelAttribute("form") Cidade cidade) throws IOException {
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/cidade");
+		
+		cidadeRepository.save(cidade);
+		return modelAndView;
+	}
+	
+	@GetMapping("/{id}/update")
+	public ModelAndView update(@PathVariable Long id, @RequestParam("cidade") String cidade,
+			@RequestParam("estado") String estado, @RequestParam("pais") String pais, @RequestParam("aero") String aero)
+			throws IOException{
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/cidade");
+		
+		Cidade cid = cidadeRepository.findById(id).get();
+		cid.setCidade(cidade);
+		cid.setEstado(estado);
+		cid.setPais(pais);
+		cid.setAeroporto(aero);
+		
+		cidadeRepository.save(cid);
+		return modelAndView;
+	}
+	
+	@GetMapping("/{id}/delete")
+	public ModelAndView delete(@PathVariable Long id) {
+		ModelAndView modelAndView = new ModelAndView("redirect:/admin/cidade");
+		
+		cidadeRepository.deleteById(id);
+		
+		return modelAndView;
+	}
+}
