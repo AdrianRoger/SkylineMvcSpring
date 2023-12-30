bca4eaa3531dba08e08ba6aacbadc62069b7b525
AdrianRoger <114889467+AdrianRoger@users.noreply.github.com>
2023-12-30 02:20:18 -0300

    [feat]: refactor CidadeController and cidade.html to RestController

--- src/main/java/com/SkylineMvcSpring/controller/CidadeController.java
+++ src/main/java/com/SkylineMvcSpring/controller/CidadeController.java
@@ -4,19 +4,21 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.SkylineMvcSpring.model.Cidade;
 import com.SkylineMvcSpring.repository.CidadeRepository;
 
-@Controller
+@RestController
 @RequestMapping("/admin/cidade")
 public class CidadeController {
 
@@ -47,29 +49,26 @@ public class CidadeController {
 	}
 	
 	//atualizar
-	@GetMapping("/{id}/update")
-	public ModelAndView update(@PathVariable Long id, @RequestParam("cidade") String cidade,
-			@RequestParam("estado") String estado, @RequestParam("pais") String pais, @RequestParam("aero") String aero)
-			throws IOException{
+	@PutMapping
+	public ModelAndView update(@ModelAttribute Cidade cidade) throws IOException{
 		ModelAndView modelAndView = new ModelAndView("redirect:/admin/cidade");
 		
-		Cidade cid = cidadeRepository.findById(id).get();
-		cid.setCidade(cidade);
-		cid.setEstado(estado);
-		cid.setPais(pais);
-		cid.setAeroporto(aero);
-		
-		cidadeRepository.save(cid);
+		cidadeRepository.save(cidade);
 		return modelAndView;
 	}
 	
 	//deletar
-	@GetMapping("/{id}/delete")
+	@DeleteMapping("/{id}")
 	public ModelAndView delete(@PathVariable Long id) {
 		ModelAndView modelAndView = new ModelAndView("redirect:/admin/cidade");
 		
-		cidadeRepository.deleteById(id);
-		
+		try {
+			cidadeRepository.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+		 
+			modelAndView.addObject("error", "Não é possível excluir o registro devido a referências no banco de dados!");	        
+			return modelAndView;
+		}
 		return modelAndView;
 	}
 }
