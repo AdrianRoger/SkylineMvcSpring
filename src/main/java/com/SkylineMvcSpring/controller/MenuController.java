337fce4ed5ab14b35b0b9fbe4775decad70543fc
AdrianRoger <114889467+AdrianRoger@users.noreply.github.com>
2023-12-31 14:03:01 -0300

    [fix]: updating contato create view to use ContatoController same ohter methods

--- src/main/java/com/SkylineMvcSpring/controller/MenuController.java
+++ src/main/java/com/SkylineMvcSpring/controller/MenuController.java
@@ -1,21 +1,16 @@
 package com.SkylineMvcSpring.controller;
 
-import java.io.IOException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.SkylineMvcSpring.model.Contato;
-import com.SkylineMvcSpring.repository.ContatoRepository;
 
 @Controller
 public class MenuController {
 	
-	@Autowired
-	private ContatoRepository contatoRepository;
+//	@Autowired
+//	private ContatoRepository contatoRepository;
 	
 	
 	@GetMapping("/destino")
@@ -37,13 +32,13 @@ public class MenuController {
 		return modelAndView;
 	}
 	
-	@PostMapping("/contato")
-	public ModelAndView cadastrarContato(Contato contato) throws IOException {
-		
-		ModelAndView modelAndView = new ModelAndView("redirect:/contato");
-	 
-		contatoRepository.save(contato);
- 
-		return modelAndView;
-	}
+//	@PostMapping("/contato")
+//	public ModelAndView cadastrarContato(Contato contato) throws IOException {
+//		
+//		ModelAndView modelAndView = new ModelAndView("redirect:/contato");
+//	 
+//		contatoRepository.save(contato);
+// 
+//		return modelAndView;
+//	}
 }
